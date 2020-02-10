676ee7767404e4b4edee74a6740c8f85bcf019dd
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2020-02-11 00:25:06 +0530

    added model Note, relation with Reviewer and Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :reviewer
+  has_many :notes
 end
--- app/models/reviewer.rb
+++ app/models/reviewer.rb
@@ -1,3 +1,4 @@
 class Reviewer < ApplicationRecord
   has_many :books
+  has_many :notes, through: :books
 end
