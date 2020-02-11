8a7932a836c4a49466b6aaf8628b582e3ec6f96d
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2020-02-11 09:53:44 +0530

    implement logic for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    reviewer = Reviewer.find_by(username: params[:reviewer][:username])
+
+    if(reviewer && reviewer.authenticate(params[:reviewer][:password]))
+      session[:reviewer_id] = reviewer.id
+      redirect_to books_path, notice: "Logged-in successfully"
+    else
+      redirect_to login_path, alert: "Invalid username/password combination"
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to login_path, notice: "Logged-out successfully"
   end
 end
