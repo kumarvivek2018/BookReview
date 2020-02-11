1f0217f88f6c0add4935824ff8659da6fc107936
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2020-02-11 07:52:56 +0530

    generate sessions controller and set up routes for login, logout, and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  get "/login", to: "sessions#new", as: "login"
+  delete "/logout", to: "sessions#destroy", as: "logout"
+
+  resources :reviewer, only: [:create]
+  get "/signup", to: "reviewer#new", as: "signup"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "books#index"
   resources :books do
