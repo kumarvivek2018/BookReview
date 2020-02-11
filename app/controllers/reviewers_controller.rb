284c9c1599dabd12d5a45e52a451eb4964be2162
Vivek Kumar Chaudhary <chaudharv101@gmail.com>
2020-02-11 10:01:58 +0530

    implemented sign up logic

--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -3,4 +3,20 @@ class ReviewersController < ApplicationController
     @reviewer = Reviewer.new
     render 'signup'
   end
+
+  def create
+    reviewer = Reviewer.new(reviewer_params)
+
+    if(reviewer.save)
+      session[:reviewer_id] = reviewer.id
+      redirect_to books_path, notice: "Account created, logged-in successfully"
+    else
+      redirect_to signup_path, alert: "Unable to create an account"
+    end
+  end
+
+  private
+    def reviewer_params
+      params.fetch(:reviewer, {}).permit(:name, :username, :password)
+    end
 end
